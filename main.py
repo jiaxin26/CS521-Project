71048f7af772a1b6bd76568ab1c1b3eeb863a3d5
AlanHou5 <107676153+AlanHou5@users.noreply.github.com>
2025-05-04 13:21:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from recip_associative_swap import swap_recip_associative
 from fuse_one_to_one_operations import fuse_elementwise_chains
 from distributive1 import DistributiveRulePass
 from distributive2 import DistributiveRule2Pass
-
+from associative2 import SqrtAssociativePass
+from commutative2 import CommutativePass
 
 def apply_all_rewrites(gm: torch.fx.GraphModule):
     # Commutative rules
     gm = swap_bitshift_reducesum(gm)
+    gm = CommutativePass()(gm)
 
     # Associative rules
     gm = swap_recip_associative(gm)
+    gm = SqrtAssociativePass()(gm)
 
     # Distributive rules
     gm = DistributiveRulePass()(gm)
